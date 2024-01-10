6518e4f1935cba1bf0d4c6b286343888b25d2b5d
Cameron Chandler <webstormcam@gmail.com>
2024-01-10 00:30:19 -0500

    Can call the API, will encode the text later lol
    It was crashing the parse after we decoded the text lol, Going to finish styling the main page then will start working on the second page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import First from './First.jsx'
 import {decode} from 'html-entities';
 
 function App() {
-  const [allQuestions, setAllQuestions] = React.useState([])
+  const [allQuestions, setAllQuestions] = React.useState({})
 
 
   React.useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=10')
        .then((response) => response.json())
        .then((data) => {
-          const capturedData = JSON.stringify(data.results)
-          const decodedText = decode(capturedData,{level:'html5'})
-          // setAllQuestions(capturedData)
-          console.log(decodedText)
+          // const capturedData = JSON.stringify(data)
+          // const decodedText = decode(capturedData,{level:'html5'})
+          // console.log(decodedText)
+          // const parsedText = JSON.parse(decodedText)
+          setAllQuestions(data.results)
+          // console.log(capturedData)
        })
        .catch((err) => {
           console.log(err.message);
@@ -21,7 +23,7 @@ function App() {
  }, []);
 
 
-
+console.log(allQuestions)
 
   return (
     <>
